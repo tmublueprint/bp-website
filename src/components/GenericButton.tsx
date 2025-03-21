f060adb45d3bcac2b533d84eb10cadb60fd791c4
Vicheka Oeun <oeunvicheka95@gmail.com>
2025-03-21 00:01:15 -0400

    remove storybook and add some basic components

---
+++ src/components/GenericButton.tsx
@@ -0,0 +1,12 @@
+function GenericButton() {
+
+    return (
+        <>
+            <button className="button bg-red-500">
+                Button
+            </button>
+        </>
+    )
+}
+
+export default GenericButton;
