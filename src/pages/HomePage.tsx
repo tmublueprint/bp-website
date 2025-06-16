99e0bde3b5fc4f47e8c50e73a8875dade37da0ee
Zachary TN <zacharytoyenakamura@gmail.com>
2025-06-16 01:10:43 -0400

    Using poppins tff files instead of random imports

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,18 +16,15 @@ import Card from "../components/Card";
 function HomePage() {
     return (
         <>
-            <GenericButton label="Contact Us"/>
             <Card>
                 <p className="text-white text-lg">Testing</p>
             </Card>
             <Card>
                 <img src="public/logo.png" alt="example" className="object-contain w-full h-full" />
             </Card>
-
-
             <ValuesCard />
-            {/*<StudentInvolvementCard></StudentInvolvementCard>*/}
-        </>
+            <StudentInvolvementCard></StudentInvolvementCard>
+        </> 
 
     )
 }
