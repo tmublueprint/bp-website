dca5a68dda41371f3c97e08a0e07d10309c4df78
kristinatruong1 <kristinatruongkyt@gmail.com>
2025-07-19 01:02:00 -0400

    retouches

--- src/components/AboutPage/JoinTeamCard.tsx
+++ src/components/AboutPage/JoinTeamCard.tsx
@@ -4,7 +4,7 @@ import GenericButton from "../../components/GenericButton";
 
 const JoinTeamCard = () => {
   return (
-    <section className="bg-white rounded-2xl py-16 px-6 md:py-24 md:px-10 text-center space-y-6">
+    <section>
       <h1 className="text-6xl font-bold text-gray-900">Join The Team!</h1>
       <p className="text-base text-gray-700 max-w-xl mx-auto">
         Passionate about using tech for good? We’re looking for Project Leads and Developers to build impactful solutions for nonprofits. Gain hands-on experience, collaborate with like-minded peers, and make a difference.
