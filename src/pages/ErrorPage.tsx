f86f5ac0efb4e32c502074d2ea352d43d1f449be
Vicheka Oeun <oeunvicheka95@gmail.com>
2025-08-19 20:25:37 -0400

    fix error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,8 @@
 function ErrorPage() {
     return (
-        <>
-            this is the error page.
-        </>
+        <div className="min-h-screen flex items-center justify-center p-8">
+            <p className="text-2xl">This is the error page.</p>
+        </div>
     )
 }
 
