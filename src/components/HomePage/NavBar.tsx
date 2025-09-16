3ce141b9d453694e4c71761c8a9b8c90c7cf9e8d
Ammar-AhmadCA <beefstr93@gmail.com>
2025-09-15 22:28:44 -0400

    Added colour to navBar to match background

--- src/components/HomePage/NavBar.tsx
+++ src/components/HomePage/NavBar.tsx
@@ -29,45 +29,47 @@ function NavBar() {
 
     return (
         <div id="navBar-container">
-            <ul id="nav-bar">
-                <button id="hamburger-button" onClick={hamburgerClick}>
-                    <img src={HamburgerIcon} alt="hamburger menu"/>
-                </button>
+            <div className="navBar-gradient">  
+                <ul id="nav-bar">
+                    <button id="hamburger-button" onClick={hamburgerClick}>
+                        <img src={HamburgerIcon} alt="hamburger menu"/>
+                    </button>
+                    
+                    <a id="home-link" href="/" onClick={(e) => handleNavigation(e, '/')}>
+                        <img src={SmallLogo} alt="bp logo" id="bp-small-logo"/>
+                    </a>
+                    <a id="home-link-full" href="/" onClick={(e) => handleNavigation(e, '/')}>
+                        <img src={FullLogo} alt="bp logo" id="navBar-bp-full-logo"/>
+                    </a>
+                    
+                    <div id="navBar-items">
+                        <li><a href="/about" onClick={(e) => handleNavigation(e, '/about')}>About Us</a></li>
+                        <li><a href="/project" onClick={(e) => handleNavigation(e, '/project')}>Projects</a></li>
+                        <li><a href="/student" onClick={(e) => handleNavigation(e, '/student')}>Students</a></li>
+                        <li><a href="/npo" onClick={(e) => handleNavigation(e, '/npo')}>Non-Profits</a></li>
+                    </div>
+                    
+                    <a id="navBar-button" href="/contact" onClick={(e) => handleNavigation(e, '/contact')}>
+                        <p id="navBar-button-text">Contact Us</p>
+                        <img src={NorthEastArrow} alt="arrow" className="white-north-east-arrow"/>
+                    </a>
+                    <a id="navBar-button-small" href="/contact" onClick={(e) => handleNavigation(e, '/contact')}>
+                        <img src={NorthEastArrow} alt="arrow" className="white-north-east-arrow-small"/>
+                    </a>
+                </ul>
                 
-                <a id="home-link" href="/" onClick={(e) => handleNavigation(e, '/')}>
-                    <img src={SmallLogo} alt="bp logo" id="bp-small-logo"/>
-                </a>
-                <a id="home-link-full" href="/" onClick={(e) => handleNavigation(e, '/')}>
-                    <img src={FullLogo} alt="bp logo" id="navBar-bp-full-logo"/>
-                </a>
-                
-                <div id="navBar-items">
-                    <li><a href="/about" onClick={(e) => handleNavigation(e, '/about')}>About Us</a></li>
-                    <li><a href="/project" onClick={(e) => handleNavigation(e, '/project')}>Projects</a></li>
-                    <li><a href="/student" onClick={(e) => handleNavigation(e, '/student')}>Students</a></li>
-                    <li><a href="/npo" onClick={(e) => handleNavigation(e, '/npo')}>Non-Profits</a></li>
-                </div>
-                
-                <a id="navBar-button" href="/contact" onClick={(e) => handleNavigation(e, '/contact')}>
-                    <p id="navBar-button-text">Contact Us</p>
-                    <img src={NorthEastArrow} alt="arrow" className="white-north-east-arrow"/>
-                </a>
-                <a id="navBar-button-small" href="/contact" onClick={(e) => handleNavigation(e, '/contact')}>
-                    <img src={NorthEastArrow} alt="arrow" className="white-north-east-arrow-small"/>
-                </a>
-            </ul>
-            
-            {<div id="hamburger-menu" className={menuOpen ? "open" : ""}>
-                <button onClick={() => setMenuOpen(false)} id="hamburger-close">
-                    <img src={CloseIcon} alt="close" id="hamburger-img"/>
-                </button>
-                <div id="hamburger-items">
-                    <div><a href="/about" onClick={(e) => handleNavigation(e, '/about')}>About Us</a></div>
-                    <div><a href="/project" onClick={(e) => handleNavigation(e, '/project')}>Projects</a></div>
-                    <div><a href="/student" onClick={(e) => handleNavigation(e, '/student')}>Students</a></div>
-                    <div><a href="/npo" onClick={(e) => handleNavigation(e, '/npo')}>Non-Profits</a></div>
-                </div>
-            </div>}
+                {<div id="hamburger-menu" className={menuOpen ? "open" : ""}>
+                    <button onClick={() => setMenuOpen(false)} id="hamburger-close">
+                        <img src={CloseIcon} alt="close" id="hamburger-img"/>
+                    </button>
+                    <div id="hamburger-items">
+                        <div><a href="/about" onClick={(e) => handleNavigation(e, '/about')}>About Us</a></div>
+                        <div><a href="/project" onClick={(e) => handleNavigation(e, '/project')}>Projects</a></div>
+                        <div><a href="/student" onClick={(e) => handleNavigation(e, '/student')}>Students</a></div>
+                        <div><a href="/npo" onClick={(e) => handleNavigation(e, '/npo')}>Non-Profits</a></div>
+                    </div>
+                </div>}
+            </div>
         </div>
     )
 }
