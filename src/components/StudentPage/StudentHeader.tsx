d5317ec37c5a33d46bf188c05bd17721c806c34b
Vicheka Oeun <oeunvicheka95@gmail.com>
2026-01-13 00:32:38 -0500

    update application link

--- src/components/StudentPage/StudentHeader.tsx
+++ src/components/StudentPage/StudentHeader.tsx
@@ -6,18 +6,18 @@ function StudentHeader() {
             <h1 className="font-['Poppins'] font-bold text-[55.3px] text-[#333]">Students</h1>
             <div className="font-['Poppins'] max-w-[843.3px] text-[20px] mt-5 text-[#333] student-header-small">
                 At Blueprint, we welcome students from all years, programs, and levels of experience at TMU. Whether you're
-                 interested in leading projects or developing tech solutions, we offer opportunities to work on impactful
-                  initiatives with nonprofits. Join us to gain real-world experience, build your skills, and make a meaningful difference.
+                interested in leading projects or developing tech solutions, we offer opportunities to work on impactful
+                initiatives with nonprofits. Join us to gain real-world experience, build your skills, and make a meaningful difference.
             </div>
             <div className="flex justify-start mt-[3rem]">
-                <GenericButton 
-                    label="Apply Now" 
-                    style={{ 
-                        marginLeft: "0", 
+                <GenericButton
+                    label="Apply Now"
+                    style={{
+                        marginLeft: "0",
                         background: "linear-gradient(90deg, #94c9ff, #76b7fb, #489cf4, #2788f0, #137ced, #0c78ec)",
                         color: "white"
                     }}
-                    to="https://concise-barberry-ea8.notion.site/Blueprint-Role-Descriptions-25805764f563808eb1c8ff7bcc4d5c9c"
+                    to="https://concise-barberry-ea8.notion.site/Blueprint-Role-Descriptions-2d905764f56381bb81c5d59ad46d7de2"
                 />
             </div>
         </div>
